5c1c558ccf947174eb1187d27e258e799a90b2f0
Blaine Lawson <blaine.lawson@gmail.com>
2020-01-13 11:56:53 -0500

    Add project files.

---
+++ lucky-number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lucky_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to find out if it is lucky: ");
+            string input = Console.ReadLine();
+
+            int.TryParse(input, out int num);
+            Console.WriteLine(num.LuckyOrNot());
+
+        }
+    }
+
+    public static class LuckyExtension
+    {
+        public static string LuckyOrNot(this int num)
+        {
+            if (num == 13 || num == 7)
+            {
+                return "Lucky";
+            }
+            else
+            {
+                return "Not lucky";
+            }
+        }
+    }
+}
